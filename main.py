630ea75b055705eaa32ce006b473eee2436680a5
MoustafaElsaid <mk.elsaid@icloud.com>
2025-04-17 11:38:09 +0200

    Modified the README.md file and tested a footnote

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init() # Initializing the game 
+    pygame.init() # Initializing the game
+    clock = pygame.time.Clock() # starting the clock
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -11,11 +14,14 @@ def main():
                 return
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip()  # refreshing the screen every frame
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+
 
 
 print ("Starting Asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
-if __name__ == "__main__":           # This makes sure that the main() function is called when the file is called directly.
+if __name__ == "__main__":           # This makes sure that the main() function is called when the file is called in the Terminal.
     main()
